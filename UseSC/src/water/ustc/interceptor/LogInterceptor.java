a68599153d5cd2e3c5e54412d68585b1a33abd78
Gordon <gordondeng@foxmail.com>
2017-12-15 11:55:47 +0800

    增加写入XML功能

--- UseSC/src/water/ustc/interceptor/LogInterceptor.java
+++ UseSC/src/water/ustc/interceptor/LogInterceptor.java
@@ -1,7 +1,15 @@
 package water.ustc.interceptor;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class LogInterceptor {
     private long s_Time;
@@ -34,9 +42,42 @@ public class LogInterceptor {
 
 
     private void afterAction(){
+
+
         XMLWriter writer = null;
         SAXReader reader = new SAXReader();
 
-        
+        SimpleDateFormat formattime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+
+        OutputFormat format = OutputFormat.createPrettyPrint();
+        format.setEncoding("GBK");
+
+        String path="/Users/gordon/GitHub/Custom-SSH/UseSC/src/log.xml";
+        e_Time = System.currentTimeMillis();
+        Document doc = DocumentHelper.createDocument();
+        Element root = doc.addElement("log");
+        Element action = root.addElement("action");
+
+        Element name = action.addElement("name");
+        name.setText(action_Name);
+
+        Element start = action.addElement("s-time");
+        start.setText(formattime.format(s_Time));
+
+        Element end = action.addElement("e-time");
+        end.setText(formattime.format(e_Time));
+
+        Element result = action.addElement("result");
+        result.setText(action_Name);;
+
+        try {
+            writer = new XMLWriter(new FileWriter(path), format);
+            writer.write(doc);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
