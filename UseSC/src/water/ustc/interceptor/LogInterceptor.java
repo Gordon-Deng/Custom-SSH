06c70e9632e8cba207323bb4f5e1f5f35e1af224
Gordon <gordondeng@foxmail.com>
2017-12-15 11:52:48 +0800

    拦截器修改

--- UseSC/src/water/ustc/interceptor/LogInterceptor.java
+++ UseSC/src/water/ustc/interceptor/LogInterceptor.java
@@ -1,11 +1,42 @@
 package water.ustc.interceptor;
 
-public class LogInterceptor {
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
 
-    private String  preAction(){
-        return "";
+public class LogInterceptor {
+    private long s_Time;
+    private long e_Time;
+    public String action_Name;
+    public String action_Result;
+
+    public String getInterceptor_ActionName() {
+        return action_Name;
     }
-    private String  afterAction(){
-        return "";
+
+    public String getInterceptor_ActionResult() {
+        return action_Result;
+    }
+
+    public void setInterceptor_ActionName(String action_Name) {
+        this.action_Name = action_Name;
+    }
+
+    public void setInterceptor_ActionResult(String action_Result) {
+        this.action_Result = action_Result;
+    }
+
+
+
+    private void preAction() throws InterruptedException {
+        s_Time = System.currentTimeMillis();
+        Thread.sleep(1000);
+    }
+
+
+    private void afterAction(){
+        XMLWriter writer = null;
+        SAXReader reader = new SAXReader();
+
+        
     }
 }
